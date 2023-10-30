36325ac23f63ba1a5f814e9b37adb404d8845122
Marcel Antoneo Ananda <marcelananda104@mhs.mdp.ac.id>
2023-10-30 10:41:12 +0700

    m

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget{
+  HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: ClipRect(),
+                ),
+              ],
+            ),
+              ],
+            )
+      //       Row(
+      //
+      //       )
+      //     ],
+      //   )
+      // )
+
+    );
+  }
+}
